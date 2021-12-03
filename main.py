544110f4640cabff978f3460a9a3f80f117aade1
Azar Aghasyev <azar.aghasiyev@gmail.com>
2021-12-03 13:16:53 -0800

    Added updated file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import date
-
+print("Hello from python to circleci")
+import os
+print(os.environ['CIRCLE_REPOSITORY_URL'])
 today = date.today()
 print("Today's date:", today)
-print("Hello from python to circleci")
